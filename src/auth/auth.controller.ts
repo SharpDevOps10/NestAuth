3b46c6dc36c56ca6868420328d23272b0f216736
SharpDevOps10 <td090833@gmail.com>
2023-10-29 17:38:10 +0200

    feature #45: added @Public decorator
    closes #45

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,8 +2,8 @@ import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs
 import { AuthService } from './auth.service';
 import { AuthDTO } from './dto';
 import { Token } from './types';
-import { AccessTokenGuard, RefreshTokenGuard } from '../common/guards';
-import { GetCurrentUser, GetCurrentUserId } from '../common/decorators';
+import { RefreshTokenGuard } from '../common/guards';
+import { GetCurrentUser, GetCurrentUserId, Public } from '../common/decorators';
 
 @Controller('auth')
 export class AuthController {
@@ -11,12 +11,14 @@ export class AuthController {
     private authService: AuthService,
   ) {}
 
+  @Public()
   @Post('/local/signup')
   @HttpCode(HttpStatus.CREATED)
   signupLocal (@Body() dto: AuthDTO): Promise<Token> {
     return this.authService.signupLocal(dto);
   }
 
+  @Public()
   @Post('/local/signin')
   @HttpCode(HttpStatus.OK)
   signinLocal (@Body() dto: AuthDTO): Promise<Token> {
--- src/common/guards/accessToken.guard.ts
+++ src/common/guards/accessToken.guard.ts
@@ -1,7 +1,20 @@
 import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { ExecutionContext } from '@nestjs/common';
 
 export class AccessTokenGuard extends AuthGuard('jwt') {
-  constructor () {
+  constructor (private reflector: Reflector) {
     super();
+  }
+
+  canActivate (context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) return true;
+
+    return super.canActivate(context);
   }
 }
